829c8decdda876a427dd4a64beaeca5cb5f9464e
Chris <36590357+Osmodia666@users.noreply.github.com>
2025-06-01 13:32:04 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const mqttConfig = {
-  host: 'www.cyberdyne.chickenkiller.com:9001',
+  host: 'wss://cyberdyne.chickenkiller.com:8884',
   username: 'christopher',
   password: 'v6Vrhy6u4reJsng',
 }
