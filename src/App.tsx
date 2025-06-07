1665700640d4f28f94776731d54ec55c825906b8
Chris <36590357+Osmodia666@users.noreply.github.com>
2025-06-07 08:40:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const client = mqtt.connect(mqttConfig.host, {
   password: mqttConfig.password,
 })
 
-client.setMaxListeners(20) // ⚠️ hinzugefügt zur Vermeidung der MaxListenersWarning
+client.setMaxListeners(50) // ⚠️ hinzugefügt zur Vermeidung der MaxListenersWarning
 
 type MinMax = Record<string, { min: number; max: number }>
 
