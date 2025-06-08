f9be4584007628c3f1849dd763a3b3bb37429943
Chris <36590357+Osmodia666@users.noreply.github.com>
2025-06-08 13:52:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -160,7 +160,7 @@ function App() {
         Letztes Update: {lastUpdate || 'Lade...'}
       </header>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
         {topics.map(({ label, type, unit, favorite, statusTopic, publishTopic, topic }) => {
           const key = statusTopic ?? topic
           let raw = values[key]
